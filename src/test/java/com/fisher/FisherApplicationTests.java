8dd4a4ee8921ee307f2917762df6a106d04405c0
GhostSharkDevotion <GhostSharkDevotion@noreply.gitcode.com>
2025-07-04 15:41:51 +0800

    first commit

---
+++ src/test/java/com/fisher/FisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
